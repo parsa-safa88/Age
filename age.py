a344f381e86440d99e3df4d462916356bdffae65
parsa safaei <124870349+parsa-safa88@users.noreply.github.com>
2023-03-02 11:16:31 +0330

    Create age.py

---
+++ age.py
@@ -0,0 +1,19 @@
+age = int(input("Please enter yout age: "))
+if (age) < 0 :
+    print ("age is wrong")
+    print ("run the program again and please enter a right age")
+elif (age) <= 12 :
+    print ("you are a child")
+elif (age) <= 18 :
+    print ("you are teenager")
+elif (age) <= 40 :
+    print ("you are young")
+elif (age) <= 65:
+    print ("you are middle age")
+elif (age) <= 90 :
+    print ("you are old")
+elif (age) <= 105:
+    print ("you are so old")
+elif (age) >= 105:
+    print ("you are so old. drop dead")
+print ("The End")
